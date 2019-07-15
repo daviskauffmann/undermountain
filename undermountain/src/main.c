c9742547219cf45ae019a346634739a452f5a73a
Davis Kauffmann <daviskauffmann@gmail.com>
2019-07-14 23:12:43 -0400

    Renaming

--- undermountain/src/main.c
+++ undermountain/src/main.c
@@ -3,18 +3,18 @@
 
 #include <platform/platform.h>
 
-int main(int argc, char *args[])
+int main(int argc, char *argv[])
 {
     for (int i = 1; i < argc; i++)
     {
-        if (strcmp(args[i], "-h") == 0 || strcmp(args[i], "--help") == 0)
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
         {
             printf("Options:\n");
             printf("  -h, --help\tPrint this message\n");
             printf("  -v, --version\tPrint current version\n");
         }
 
-        if (strcmp(args[i], "-v") == 0 || strcmp(args[i], "--version") == 0)
+        if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--version") == 0)
         {
             printf("%s %s\n", TITLE, VERSION);
             printf("%s\n", TCOD_STRVERSIONNAME);

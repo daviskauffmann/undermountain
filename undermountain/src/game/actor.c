1d13937f4b6023d4db83727f14988b3813691583
Davis Kauffmann <daviskauffmann@gmail.com>
2019-08-22 02:08:41 -0400

    Remove unused include

--- undermountain/src/game/actor.c
+++ undermountain/src/game/actor.c
@@ -4,7 +4,6 @@
 #include <float.h>
 #include <malloc.h>
 #include <math.h>
-#include <stdio.h>
 #include <string.h>
 
 #include "assets.h"
--- undermountain/src/game/util.c
+++ undermountain/src/game/util.c
@@ -6,7 +6,6 @@ float distance_between_sq(int x1, int y1, int x2, int y2)
 {
     float dx = (float)(x1 - x2);
     float dy = (float)(y1 - y2);
-
     return powf(dx, 2) + powf(dy, 2);
 }
 
@@ -19,18 +18,15 @@ float angle_between(int x1, int y1, int x2, int y2)
 {
     float dx = (float)(x1 - x2);
     float dy = (float)(y1 - y2);
-
     return atan2f(dy, dx);
 }
 
 int roll(int a, int x)
 {
     int result = 0;
-
     for (int i = 1; i <= a; i++)
     {
         result += TCOD_random_get_int(NULL, 1, x);
     }
-
     return result;
 }
